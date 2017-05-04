13ce01dfa96c8c71db2aefeb26ce014adfe1d186
Tymoteusz24 <tymoteusz.pasieka@gmail.com>
2017-05-04 18:15:14 +0200

    facebook login

--- TimSocial/SingInVC.swift
+++ TimSocial/SingInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,32 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookPressed(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("TIM: Unable to auth with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("TIM: USer Cancel facebook auth")
+            } else {
+                print("TIM: Success auth with fb")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+    }
+    
+    func firebaseAuthenticate(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("TIM: Unable to auth with Firebase - \(String(describing: error))")
+            } else {
+                print("TIM: Succecfully auth with Firebase")
+            }
+        })
+        
+    }
 
 }
 
