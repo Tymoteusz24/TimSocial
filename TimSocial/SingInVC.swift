dfcb6174feb7ec0396a1a593fcfd9971d9d19b28
Tymoteusz24 <tymoteusz.pasieka@gmail.com>
2017-05-04 23:33:23 +0200

    firebase login

--- TimSocial/SingInVC.swift
+++ TimSocial/SingInVC.swift
@@ -12,6 +12,8 @@ import FBSDKCoreKit
 import Firebase
 
 class SignInVC: UIViewController {
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +41,7 @@ class SignInVC: UIViewController {
         }
     }
     
+    
     func firebaseAuthenticate(_ credential: FIRAuthCredential){
         FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
             if error != nil {
@@ -49,6 +52,27 @@ class SignInVC: UIViewController {
         })
         
     }
+    
+    @IBAction func emailLoginPressed(_ sender: Any) {
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("TIMI: Email user auth with firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Timi: Email user failed auth with FIREBASE")
+                        } else {
+                            print("TIMI: Succesfully created account with firebase")
+                        }
+                    })
+                    
+                }
+            })
+        }
+        
+    }
+    
 
 }
 
